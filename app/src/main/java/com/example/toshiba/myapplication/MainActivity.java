f828124320250df5fb218292f9a10438af0d3b2b
LhllhL133 <836290755@qq.com>
2017-03-07 16:02:48 +0800

    11111

--- app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
+++ app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.toshiba.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button q1=(Button)findViewById(R.id.q1);
+        q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.t0);
+            }
+        });
+
+        Button q2=(Button)findViewById(R.id.q2);
+        q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.t1);
+            }
+        });
+
+        Button q3=(Button)findViewById(R.id.q3);
+        q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.t2);
+            }
+        });
     }
 }
